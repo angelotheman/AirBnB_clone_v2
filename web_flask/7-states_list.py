8cb0c12c68d55925f56c634f66eb4ebc43737a47
Kwabena Twumasi <ktwum721@gmail.com>
2024-03-26 13:07:06 +0000

    States lists

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,6 +2,8 @@
 """
 A script to start Flask application
 """
+
+
 from flask import Flask, render_template
 from models import storage
 
@@ -10,14 +12,17 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown_session(exception):
+    """A tear down session"""
     storage.close()
 
 
 @app.route("/state_list", strict_slashes=False)
 def state_list():
+    """A state list function"""
     states = storage.all('State').values()
     return render_template('7-states_list.html', states=states)
 
 
 if __name__ == '__main__':
+    """This is a main function"""
     app.run(host='0.0.0.0', port='5000')
